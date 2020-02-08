df03fdfa0d8390cf92ad73bbdbb1cf8ce4052b33
Sridhar Nathani <Sridhar.Nathani@videa.tv>
2020-02-08 00:01:15 -0500

    Basic solution extended to be unit test friendly

---
+++ FizzBuzz/Basic/FizzBuzzBasic2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FizBuzz
+{
+    public class FizzBuzzBasic2
+    {
+        public void Run()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(PrintFizzBuzz(i));
+            }
+        }
+
+        public string PrintFizzBuzz(int number)
+        {
+            if (IsFizz(number) && IsBuzz(number))
+            {
+                return "FizBuzz";
+            }
+            else if (IsFizz(number))
+            {
+                return "Fiz";
+            }
+            else if (IsBuzz(number))
+            {
+                return "Buzz";
+            }
+            return number.ToString();
+        }
+
+        public bool IsFizz(int number)
+        {
+            return (number % 3 == 0);
+        }
+
+        public bool IsBuzz(int number)
+        {
+            return (number % 5 == 0);
+        }
+    }
+}
--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,7 @@ namespace FizBuzz
     {
         static void Main(string[] args)
         {
-            var basic = new FizzBuzzBasic();
+            var basic = new FizzBuzzBasic2();
             basic.Run();
 
             Console.Write("\r\nPress ANY key to exit");
